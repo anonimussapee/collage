877d47a07566e4b04f9652ad51ff09b50b8d0a57
John Córdova <librededrogas420@gmail.com>
2025-01-05 16:13:39 -0500

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import liz1 from './assets/1717152931002.jpg'
 import liz2 from './assets/1717152931012.jpg'
 import './App.css'
 
 function App() {
   const [smsPosition,setSmsPosition] = useState(0)
   const sms = [
-    'Te pienso mucho, muchisimo', 'y quiero decirte que','Cuando mis ojos te vieron', 'casi me caigo, casi me, casi me muero', 'cuando mis ojos te vieron', 'no solo te vieron', 'si no que al amor conocieron '
+    'Hola liz, Te pienso mucho, muchisimo', 'y quiero decirte que','eres lo mejor que me ha sucedido en la vida', 'no esperaba que existiera alguien tan especial como tu', 'la verdad, es que te amo <3', 'y por lo cursi de esta web, se nota que es asi', 'quiero que seas feliz y que te vaya bien en todo', 'te quiero un monton y espero poder seguir provocandote esa bonita sonrisa tuya', 'que tanto me gusta, y me enamora <3'
   ]
   const setSmsButton = () =>{
     if(smsPosition === sms.length-1){
@@ -24,10 +22,10 @@ function App() {
           <img src={liz2} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>{sms[smsPosition]}</h1>
+      <h2>{sms[smsPosition]}</h2>
       <div className="card">
         <button onClick={() => setSmsButton()}>
-          Hey! dame click
+         {sms.length-1 === smsPosition ? 'Volver a empezar': ' Hey! dame click'}
         </button>
       </div>
     </>
